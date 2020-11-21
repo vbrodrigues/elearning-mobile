12667ed30533973fa870b919959e2fdd8ee2f748
vbrodrigues <vitorborbarodrigues@gmail.com>
2020-11-21 14:26:53 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+
+const App: React.FC = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#6548A3" />
+    <View />
+  </>
+);
+
+export default App;
